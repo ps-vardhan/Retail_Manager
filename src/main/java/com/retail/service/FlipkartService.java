903ee3c440a49060a1a3b8fe9a7340c35a47fd66
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-02-26 14:53:22 +0530

    initialize Flipkart API service for external catalog integration

---
+++ src/main/java/com/retail/service/FlipkartService.java
@@ -0,0 +1,11 @@
+package com.retail.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlipkartService {
+
+    public void syncAllProducts() {
+        // Logic to siphon data from Flipkart API
+    }
+}
