ada976f2e55abdf57cb35a9a6956870cfce0d7ca
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-03-18 19:33:49 +0530

    Upgraded version for springai

--- central/src/central/java/com/retail/retailmanager/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.retail.retailmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.retail.retailmanager.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- central/src/central/java/com/retail/retailmanager/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.retail.retailmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.retail.retailmanager.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
